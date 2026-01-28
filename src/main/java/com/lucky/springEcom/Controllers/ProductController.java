03303563dd89be3c941e3b67c75d215de9db8b0f
Lakshay Chauhan <lakshaychauhan129@gmail.com>
2026-01-28 22:51:32 +0530

    Added new Methods

--- src/main/java/com/lucky/springEcom/Controllers/ProductController.java
+++ src/main/java/com/lucky/springEcom/Controllers/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -31,5 +33,49 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(product , HttpStatus.OK);
+    }
+
+
+    @GetMapping("/product/{id}/image")
+    public ResponseEntity<?> getImageData(@PathVariable int id){
+        Product product = productService.getProductById(id);
+        if(product !=null ){
+            return new ResponseEntity<>(product.getImageData(), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product , @RequestPart MultipartFile image){
+
+        try{
+             return new ResponseEntity<>(productService.addOrUpdateProduct(product, image), HttpStatus.OK);
+        }catch (IOException e){
+            return new ResponseEntity<>(e.getLocalizedMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PutMapping("/product")
+    public ResponseEntity<String> updateProduct(@RequestPart Product product , @RequestPart MultipartFile image){
+        try{
+            productService.addOrUpdateProduct(product, image);
+            return new ResponseEntity<>("Updated Successfully", HttpStatus.OK);
+        }catch (IOException e){
+            return new ResponseEntity<>(e.getLocalizedMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+
+        boolean success = productService.deleteProductById(id);
+
+        if(success){
+             return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Error in Deletion", HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/lucky/springEcom/Services/ProductService.java
+++ src/main/java/com/lucky/springEcom/Services/ProductService.java
@@ -4,8 +4,11 @@ import com.lucky.springEcom.Models.Product;
 import com.lucky.springEcom.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -19,5 +22,27 @@ public class ProductService {
 
     public Product getProductById(int id) {
         return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addOrUpdateProduct(Product product , MultipartFile image) throws IOException {
+
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return productRepository.save(product);
+
+    }
+
+    public boolean deleteProductById(int id){
+
+        Optional<Product> product = productRepository.findById(id);
+
+        if(product.isPresent()){
+            productRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
     }
 }
